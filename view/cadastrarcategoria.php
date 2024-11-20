7ad09b960aa829f0f047749f545f08ec66ee873a
luisfelip3 <luisfelipeaquino001@gmail.com>
2024-11-20 17:27:38 -0300

    Correção de Conflitos

--- view/cadastrarcategoria.php
+++ view/cadastrarcategoria.php
@@ -9,19 +9,29 @@ include('protect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Estilos/categoria.css">
     <title>Document</title>
 </head>
 <body>
-<h1>Cadastrar Categoria de produtos</h1>
-<form action="index.php?acao=cadastrarCategoria" method="POST">
-        <label for="txtcodigoCategoria">Código da Categoria:</label>
-        <input type="number" id="txtcodigoCategoria" name="txtcodigoCategoria" 
-               value="<?php echo isset($categorias ['codigo']) ? $categorias ['codigo'] : ''; ?>" required><br><br>
-
-        <label for="txtcategoria">Nome da categoria:</label>
-        <input type="text" id="txtcategoria" name="txtcategoria" 
-               value="<?php echo isset($categorias ['categoria']) ? $categorias ['categoria'] : ''; ?>" required><br><br>
-
-               <button type="submit">Salvar </button>
+<div class="container">
+       <h1>Cadastrar Categoria de produtos</h1>
+       <form action="index.php?acao=cadastrarCategoria" method="POST">
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtcodigoCategoria">Código da Categoria:</label>
+                             <input type="number" id="txtcodigoCategoria" name="txtcodigoCategoria"
+                                    value="<?php echo isset($categorias ['codigo']) ? $categorias ['codigo'] : ''; ?>" required><br><br>
+                      </div>
+               </div>
+       
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtcategoria">Nome da categoria:</label>
+                             <input type="text" id="txtcategoria" name="txtcategoria"
+                                    value="<?php echo isset($categorias ['categoria']) ? $categorias ['categoria'] : ''; ?>" required><br><br>
+                      </div>
+               </div>
+                      <div class="submits"><button class="submit" type="submit">Salvar </button></div>
+</div>
 </body>
 </html>
--- view/cadastrarfuncionario.php
+++ view/cadastrarfuncionario.php
@@ -11,54 +11,85 @@ protegePaginaGerente();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Estilos/cadastrarFuncionario.css">
     <title>Alterar Funcionário</title>
 </head>
 <body>
-    <h1>Alteração de Funcionário</h1>
-    <form action="index.php?acao=cadastrarFuncionario" method="POST">
-        <label for="txtcodigofuncionario">Código do Funcionário:</label>
-        <input type="text" id="txtcodigofuncionario" name="txtcodigofuncionario" 
-               value="<?php echo isset($funcionarios['codigo']) ? $funcionarios['codigo'] : ''; ?>" readonly><br><br>
-
-        <label for="txtnomefuncionario">Nome:</label>
-        <input type="text" id="txtnomefuncionario" name="txtnomefuncionario" 
-               value="<?php echo isset($funcionarios['nome']) ? $funcionarios['nome'] : ''; ?>"><br><br>
-
-        <label for="txtemailfuncionario">Email:</label>
-        <input type="email" id="txtemailfuncionario" name="txtemailfuncionario" 
-               value="<?php echo isset($funcionarios['email']) ? $funcionarios['email'] : ''; ?>"><br><br>
-
-        <label for="txtloginfuncionario">Login:</label>
-        <input type="text" id="txtloginfuncionario" name="txtloginfuncionario" 
-               value="<?php echo isset($funcionarios['login']) ? $funcionarios['login'] : ''; ?>"><br><br>
-
-        <label for="txtsenhafuncionario">Senha:</label>
-        <input type="text" id="txtsenhafuncionario" name="txtsenhafuncionario" 
-               value="<?php echo isset($funcionarios['senha']) ? $funcionarios['senha'] : ''; ?>"><br><br>
-
-        <label for="txttelefonefuncionario">Telefone:</label>
-        <input type="text" id="txttelefonefuncionario" name="txttelefonefuncionario" 
-               value="<?php echo isset($funcionarios['telefone']) ? $funcionarios['telefone'] : ''; ?>"><br><br>
-               
-        <label for="txtcpffuncionario">CPF:</label>
-        <input type="text" id="txtcpffuncionario" name="txtcpffuncionario" 
-               value="<?php echo isset($funcionarios['cpf']) ? $funcionarios['cpf'] : ''; ?>"><br><br>
-        <label for="txtcargofuncionario">cargo:</label>
-
-        <input type="text" id="txtcargofuncionario" name="txtcargofuncionario" 
-               value="<?php echo isset($funcionarios['cargo']) ? $funcionarios['cargo'] : ''; ?>"><br><br>
-
-        <label for="generofuncionario">Gênero:</label>
-        <select id="generofuncionario" name="generofuncionario" required>
-            <?php 
-            foreach ($generos as $gr) { 
-              $selected = (isset($funcionarioData['cod_genero']) && $funcionarioData['cod_genero'] == $gr['codigo']) ? "selected" : "";
-              echo "<option value='" . $gr['codigo'] . "' $selected>" .  $gr['genero'] . "</option>";
-            } 
-            ?>
-        </select><br><br>
-
-        <button type="submit">Salvar </button>
-    </form>
+    <div class="container">
+           <h1>Alteração de Funcionário</h1>
+           <form action="index.php?acao=cadastrarFuncionario" method="POST">
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtcodigofuncionario">Código do Funcionário:</label>
+                             <input type="text" id="txtcodigofuncionario" name="txtcodigofuncionario"
+                                    value="<?php echo isset($funcionarios['codigo']) ? $funcionarios['codigo'] : ''; ?>" readonly><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtnomefuncionario">Nome:</label>
+                             <input type="text" id="txtnomefuncionario" name="txtnomefuncionario"
+                                    value="<?php echo isset($funcionarios['nome']) ? $funcionarios['nome'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtemailfuncionario">Email:</label>
+                             <input type="email" id="txtemailfuncionario" name="txtemailfuncionario"
+                                    value="<?php echo isset($funcionarios['email']) ? $funcionarios['email'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtloginfuncionario">Login:</label>
+                             <input type="text" id="txtloginfuncionario" name="txtloginfuncionario"
+                                    value="<?php echo isset($funcionarios['login']) ? $funcionarios['login'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtsenhafuncionario">Senha:</label>
+                             <input type="text" id="txtsenhafuncionario" name="txtsenhafuncionario"
+                                    value="<?php echo isset($funcionarios['senha']) ? $funcionarios['senha'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txttelefonefuncionario">Telefone:</label>
+                             <input type="text" id="txttelefonefuncionario" name="txttelefonefuncionario"
+                                    value="<?php echo isset($funcionarios['telefone']) ? $funcionarios['telefone'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+           
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtcpffuncionario">CPF:</label>
+                             <input type="text" id="txtcpffuncionario" name="txtcpffuncionario"
+                                    value="<?php echo isset($funcionarios['cpf']) ? $funcionarios['cpf'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="txtcargofuncionario">cargo:</label>
+                             <input type="text" id="txtcargofuncionario" name="txtcargofuncionario"
+                                    value="<?php echo isset($funcionarios['cargo']) ? $funcionarios['cargo'] : ''; ?>"><br><br>
+                      </div>
+               </div>
+               <div class="inputs">
+                      <div class="input_label">
+                             <label for="generofuncionario">Gênero:</label>
+                             <select id="generofuncionario" name="generofuncionario" required>
+                                 <?php
+                                 foreach ($generos as $gr) {
+                                   $selected = (isset($funcionarioData['cod_genero']) && $funcionarioData['cod_genero'] == $gr['codigo']) ? "selected" : "";
+                                   echo "<option value='" . $gr['codigo'] . "' $selected>" .  $gr['genero'] . "</option>";
+                                 }
+                                 ?>
+                             </select><br><br>
+                      </div>
+               </div>
+               <div class="submits"><button type="submit" class="submit">Salvar </button></div>
+           </form>
+    </div>
 </body>
 </html>
