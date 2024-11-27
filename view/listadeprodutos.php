686f82c4972d61a0367a8df32f60ca6349a9f5a7
Livea Brito <liveabrito49@yahoo.com>
2024-11-27 15:27:46 -0300

    atualização das telas listafuncionarios e listadeprodutos

--- view/listadeprodutos.php
+++ view/listadeprodutos.php
@@ -8,11 +8,12 @@ include('protect.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/Estilos/listadeProdutos.css">
     <title>Lista de Produtos</title>
 </head>
 <body>
 
-    <h1>Lista de Produtos</h1>
+    <h1 class="titulo1">Lista de Produtos</h1>
 
     <a href="index.php?acao=paginacadastrar"><button>Cadastrar Produtos</button></a><br><br>
     <a href="index.php?acao=paginacadastrarcategoria"><button>Cadastrar Categorias</button></a>
